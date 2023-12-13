8fe13fff61c44d15b001fc5067ea071dc51251d5
daniblu <mr.daniblu@gmail.com>
2023-12-13 19:06:00 +0100

    del data_prep, rnm dict->data_prep

--- src/data_prep.py
+++ src/data_prep.py
@@ -1,12 +1,13 @@
 '''
 This script loads the HANNA dataset and prepares it for the model. 
-This involves averaging human annotations, train-test splitting, and evaluating label distributions within splits.
+This involves averaging human annotations, train-test splitting, and organsing the data in a Dataset object. 
 '''
 
 from pathlib import Path
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from datasets import Dataset
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pickle
@@ -16,7 +17,7 @@ if __name__ == '__main__':
     # paths
     data_path = Path(__file__).parents[1] / 'hanna_stories_annotations.csv'
     plot_path = Path(__file__).parents[1] / 'plots'
-    output_path = Path(__file__).parents[1] / 'story_eval_dataset.pkl'
+    output_path = Path(__file__).parents[1] / 'story_eval_dataset_dict.pkl'
 
     # load data
     data = pd.read_csv(data_path)
@@ -31,8 +32,8 @@ if __name__ == '__main__':
     Complexity=('Complexity', 'mean')
     )
 
-    # isolate scores
-    y = data_agg.iloc[:, 5:10]
+    # combine scores to single label
+    y = data_agg.iloc[:, 5:11].agg(func=np.array, axis=1)
 
     # isolate stories
     X = data_agg.iloc[:, 3]
@@ -46,11 +47,10 @@ if __name__ == '__main__':
     # plot distribution of scores in train and validation sets for each metric
     metrics = ['Coherence', 'Empathy', 'Surprise', 'Engagement', 'Complexity']
 
-    # plot distribution of scores in train and validation sets for each metric
-    fig, ax = plt.subplots(5, 2, figsize=(8, 24))
+    fig, ax = plt.subplots(5, 2, figsize=(8, 20))
     for i, metric in enumerate(metrics):
-        sns.histplot(y_train[metric], bins=np.arange(0, 5+(1/3), 1/3), ax=ax[i, 0]).set(xlabel=None)
-        sns.histplot(y_val[metric], bins=np.arange(0, 5+(1/3), 1/3), ax=ax[i, 1]).set(xlabel=None)
+        sns.histplot([score[i] for score in y_train], bins=np.arange(0, 5+(1/3), 1/3), ax=ax[i, 0]).set(xlabel=None)
+        sns.histplot([score[i] for score in y_val], bins=np.arange(0, 5+(1/3), 1/3), ax=ax[i, 1]).set(xlabel=None)
         ax[i, 0].set_title(f'Train_{metric}')
         ax[i, 0].set_xlim(1, 5)
         ax[i, 1].set_title(f'Validation_{metric}')
@@ -58,23 +58,26 @@ if __name__ == '__main__':
     plt.savefig(plot_path / 'train_val_score_dist.png')
 
     # plot distribution of scores in train and test sets for each metric
-    fig, ax = plt.subplots(5, 2, figsize=(8, 24))
+    fig, ax = plt.subplots(5, 2, figsize=(8, 20))
     for i, metric in enumerate(metrics):
-        sns.histplot(y_train[metric], bins=np.arange(0, 5+(1/3), 1/3), ax=ax[i, 0]).set(xlabel=None)
-        sns.histplot(y_test[metric], bins=np.arange(0, 5+(1/3), 1/3), ax=ax[i, 1]).set(xlabel=None)
+        sns.histplot([score[i] for score in y_train], bins=np.arange(0, 5+(1/3), 1/3), ax=ax[i, 0]).set(xlabel=None)
+        sns.histplot([score[i] for score in y_test], bins=np.arange(0, 5+(1/3), 1/3), ax=ax[i, 1]).set(xlabel=None)
         ax[i, 0].set_title(f'Train_{metric}')
         ax[i, 0].set_xlim(1, 5)
         ax[i, 1].set_title(f'Test_{metric}')
         ax[i, 1].set_xlim(1, 5)
     plt.savefig(plot_path / 'train_test_score_dist.png')
 
-    # combine X and y in dataframe
-    train = pd.concat([X_train, y_train], axis=1).reset_index(drop=True)
-    test = pd.concat([X_test, y_test], axis=1).reset_index(drop=True)
-    validation = pd.concat([X_val, y_val], axis=1).reset_index(drop=True)
+    # create Dataset objects
+    train_dict = {'label': y_train, 'text': X_train}
+    ds_train = Dataset.from_dict(train_dict)
+    test_dict = {'label': y_test, 'text': X_test}
+    ds_test = Dataset.from_dict(test_dict)
+    val_dict = {'label': y_val, 'text': X_val}
+    ds_val = Dataset.from_dict(val_dict)
 
-    # combine into list
-    dataset = [train, validation, test]
+    # combine into dictionary
+    dataset = {'train': ds_train, 'test': ds_test, 'validation': ds_val}
 
     # save dataset as pkl file
     with open(output_path, 'wb') as f:
